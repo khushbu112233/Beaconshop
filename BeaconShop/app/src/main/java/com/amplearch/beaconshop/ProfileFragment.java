742598e2dcdee89954750aa87080058dc285758f
kajal <kajal.amplearch@gmail.com>
2017-03-30 11:06:32 +0530

    Profile Design (complete) DatePicker Code to select Birthdate

--- BeaconShop/app/src/main/java/com/amplearch/beaconshop/ProfileFragment.java
+++ BeaconShop/app/src/main/java/com/amplearch/beaconshop/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.amplearch.beaconshop;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,9 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-public class ProfileFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+import java.util.Calendar;
+
+public class ProfileFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener{
+
+    Button btnDatePicker;
+    EditText txtDate;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
     public ProfileFragment() {
     }
 
@@ -23,6 +34,10 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
 
 
         Spinner spinner = (Spinner) rootView.findViewById(R.id.gender_spinner);
+
+        btnDatePicker=(Button) rootView.findViewById(R.id.btn_date);
+        txtDate=(EditText) rootView.findViewById(R.id.in_date);
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.gender_array, android.R.layout.simple_spinner_item);
@@ -31,6 +46,9 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        btnDatePicker.setOnClickListener(this);
+
         return rootView;
     }
 
@@ -42,6 +60,32 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 }
 
