1c37e0a52666540d4bcd2fc8b685b76abc2b5c3a
kajal <kajal.amplearch@gmail.com>
2017-03-30 19:15:35 +0530

    OfferListing Design

--- BeaconShop/app/src/main/java/com/amplearch/beaconshop/OffersListActivity.java
+++ BeaconShop/app/src/main/java/com/amplearch/beaconshop/OffersListActivity.java
@@ -3,14 +3,14 @@ package com.amplearch.beaconshop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ListView;
 
-public class StoreListActivity extends AppCompatActivity {
+public class OffersListActivity extends AppCompatActivity {
 
-    GridView storeGrid;
+    ListView listOffer;
     String[] web;
     int[] imageId = {
             R.drawable.ic_sale,
@@ -22,33 +22,26 @@ public class StoreListActivity extends AppCompatActivity {
             R.drawable.ic_sale
 
     };
-    private Toolbar topToolBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_store_list);
-
-        topToolBar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(topToolBar);
-        // topToolBar.setLogo(R.mipmap.ic_launcher);
-        topToolBar.setLogoDescription("Stores");
-        getSupportActionBar().setTitle("Stores");
+        setContentView(R.layout.activity_offers_list);
 
         web = new String[imageId.length];
-        storeGrid =(GridView) findViewById(R.id.store_list);
-        StoreListAdapter adapterViewAndroid = new StoreListAdapter(StoreListActivity.this, web, imageId);
-        storeGrid.setAdapter(adapterViewAndroid);
-        storeGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listOffer =(ListView) findViewById(R.id.offers_list);
+        OffersAdapter adapterViewAndroid = new OffersAdapter(OffersListActivity.this, web, imageId);
+        listOffer.setAdapter(adapterViewAndroid);
+        listOffer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int i, long id) {
                 //  Toast.makeText(getContext(), "GridView Item: " , Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getApplicationContext(), OffersListActivity.class);
-                startActivity(intent);
+               /* Intent intent = new Intent(getApplicationContext(), StoreListActivity.class);
+                startActivity(intent);*/
             }
         });
-
     }
 }
