a88b054089e8c21a8a2bf9ee9f5f8ed5956b8de6
kajal <kajal.amplearch@gmail.com>
2017-04-01 18:31:10 +0530

    Validations Completed

--- BeaconShop/app/src/main/java/com/amplearch/beaconshop/Activity/SignUpActivity.java
+++ BeaconShop/app/src/main/java/com/amplearch/beaconshop/Activity/SignUpActivity.java
@@ -2,14 +2,55 @@ package com.amplearch.beaconshop.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amplearch.beaconshop.R;
+import com.amplearch.beaconshop.Utils.Validations;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    public static EditText etUserName, etEmailAddress, etContactNo, etPassword, etRePassword;
+    TextView tvSignup ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etEmailAddress = (EditText) findViewById(R.id.etEmailAddress);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etRePassword = (EditText) findViewById(R.id.etRePassword);
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etContactNo = (EditText)findViewById(R.id.etContactNo);
+
+        tvSignup = (TextView) findViewById(R.id.tvSignUp);
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String UserName = etUserName.getText().toString();
+                String EmailAddress = etEmailAddress.getText().toString();
+                String ContactNo = etContactNo.getText().toString();
+                String Password = etPassword.getText().toString();
+                String RePassword = etRePassword.getText().toString();
+
+                if (!Validations.validate(UserName,EmailAddress,ContactNo,Password,RePassword))
+                {
+
+                    Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "you can go ", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
     }
 }
