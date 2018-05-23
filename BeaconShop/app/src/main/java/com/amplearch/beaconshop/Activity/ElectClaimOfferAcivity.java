10bfaef59e34f64db3a6827294579a3a831fb399
Kajal <kajal.patadia@ample-arch.com>
2018-05-23 14:55:35 +0530

    +solve image crash

--- BeaconShop/app/src/main/java/com/amplearch/beaconshop/Activity/ElectClaimOfferAcivity.java
+++ BeaconShop/app/src/main/java/com/amplearch/beaconshop/Activity/ElectClaimOfferAcivity.java
@@ -117,10 +117,13 @@ public class ElectClaimOfferAcivity extends AppCompatActivity implements View.On
         ivShare.setOnClickListener(this);
         ivFavorite.setOnClickListener(this);
         btnItemClaimOffer.setOnClickListener(this);
-        byte[] decodedString = Base64.decode(offer_image, Base64.DEFAULT);
-        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        imgOffer.setImageBitmap(decodedByte);
+        try {
+            byte[] decodedString = Base64.decode(offer_image, Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+            imgOffer.setImageBitmap(decodedByte);
+        } catch (Exception e){}
         final HashMap<String, String> user1 = session.getUserDetails();
 
         // get name
